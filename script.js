f8d658daff3ee27e27d2ead5c7484695b77c8f8d
Hania Samad <141482243+haniasamad@users.noreply.github.com>
2024-09-28 18:39:53 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+let name1 = prompt ("what's your name");
+let name2 = prompt ("2+2");
+let name3 = prompt ("what's your location");
+let name4 = prompt ("what's your fvrt food");
+
+Array= []
+Array.push(name1 , name2, name3 ,name4);
+console.log(Array);
+
+
+
